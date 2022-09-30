11fe6451ee7463294edf8d4534ccedac001c8977
voolyvex <1joeknapp@gmail.com>
2022-09-30 09:21:04 -0500

    create main and import random. print story text

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+
+def main():
+
+    print("""
+        \nYou are Hercules, the greatest of the Greek Heroes!
+You have been tasked by King Eurystheus to slay the vicious Nemean Lion, defeat the
+impossible nine-headed Lernaean Hydra, and capture the guard dog of the underworld — Cerberus.\n
+""")
+
+main()
